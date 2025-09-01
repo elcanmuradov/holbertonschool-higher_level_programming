a9b5e570a27f88c0272aca833f1114a0d1ed62b4
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-01 13:21:41 +0000

    5

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[40:72] + " " + str[113:118] + str[:5]
+print(str)
