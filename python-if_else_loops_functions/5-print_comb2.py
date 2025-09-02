2bf4203c48fc7284cdd87d64c71536b8a17d5d40
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-02 13:10:20 +0000

    5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 10:
-        print("{0}".format(i), end=", ", sep=", ")
+        print("{0}".format(i), sep=", ")
     else:
         print("{}".format(i), sep=", ")
