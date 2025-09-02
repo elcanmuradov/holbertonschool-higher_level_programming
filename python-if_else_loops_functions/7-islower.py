46420ac0b26b21c5f69f7b33026662cbce15cdd3
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-02 13:32:53 +0000

    5

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
 def islower(c):
-    if c >= "a" and c <= "z" and c != None:
+    if not isinstance(c, str) or len(c) != 1:
+        raise TypeError("Expected a single character string")
+    if "a" <= c <= "z":
         return True
     else:
         return False
