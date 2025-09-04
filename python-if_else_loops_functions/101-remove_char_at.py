2fe976fe970e1ed6cca38cb81412bf7dc4754551
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-04 13:59:53 +0000

    4

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    string = ""
-    for i in str:
-        if str.rinxdex(i) == n:
-            string += i
+    str = list(str)
+    str.pop(n)
+    string = "".join(str)
     return string
