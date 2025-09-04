b41d2aae7043ea8b027499c723212df37eb8a93b
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-04 14:33:02 +0000

    4

---
+++ python-import_modules/1-calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from calculator_1 import sub
+from calculator_1 import div
+from calculator_1 import mul
+from calculator_1 import add
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}\n".format(a, b, add(a, b)))
+    print("{} - {} = {}\n".format(a, b, sub(a, b)))
+    print("{} * {} = {}\n".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
