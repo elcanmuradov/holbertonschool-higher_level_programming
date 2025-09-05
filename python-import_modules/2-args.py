c28aca2576d5cb8643f0511e67645cdd57b6f9b4
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-05 14:02:56 +0000

    5

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
+import sys
+
 if __name__ == "__main__":
-    count = len(*args)
+    argv = sys.argv
+    count = len(argv)
     if count > 1:
         print("{} arguments:".format(count))
         for i in (1,count + 1):
-            print("{}: {}".format(i,args[i]))
+            print("{}: {}".format(i,argv[i]))
     elif count == 1:
         print("{} argument:".format(count))
         print("{}: {}".format(count,argv[1]))
