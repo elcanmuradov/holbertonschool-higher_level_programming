46de87e6c04bfab8634fd8784ad170fef678e616
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-05 14:07:14 +0000

    5

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     count = len(argv) - 1
     if count > 1:
         print("{} arguments:".format(count))
-        for i in range(1,count + 1):
-            print("{}: {}".format(i,argv[i]))
+        for i in range(1, count + 1):
+            print("{}: {}".format(i, argv[i]))
     elif count == 1:
         print("{} argument:".format(count))
-        print("{}: {}".format(count,argv[1]))
+        print("{}: {}".format(count, argv[1]))
     else:
         print("{} arguments.".format(count))
