0b4fac8f468bf0e17754bac2aceedd95b8c4c8d2
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-05 13:05:10 +0000

    5

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-count = len(argv)
-for i in (1,count + 1):
-    print("{a: argv}".format(i,args[i]))
+
+if __name__ == "__main__":
+    count = len(argv)
+    if count > 1:
+        print("{a} arguments:".format(count))
+        for i in (1,count + 1):
+            print("{a}: {argv}".format(i,args[i]))
+    elif count == 1:
+        print("{a} argument:".format(count))
+        print("{a}: {arg}".format(count,argv[1]))
+    else:
+        print("{a} arguments.".format(count))
