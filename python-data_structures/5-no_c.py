e8762ac252e31c4ddb87b8ab8b82d5468cd7f020
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-08 15:03:52 +0000

    4

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    my_string = my_string.list()
+    my_string = list(my_string)
     for i in range(0, len(my_string)):
         if my_string[i] == "c" or my_string[i] == "C":
             del my_string[i]
