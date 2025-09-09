e94c6c0db1a8400af131501e727477e7fc264e38
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-09 13:27:57 +0000

    4

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,8 +4,6 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
     new_list = my_list[:idx] + my_list[idx + 1:]
-    
     my_list.clear()
     my_list.extend(new_list)
-    
     return my_list
