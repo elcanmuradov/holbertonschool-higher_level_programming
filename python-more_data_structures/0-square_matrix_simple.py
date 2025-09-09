1e33e874595b51550d20619d8235a7e89d26f914
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-09 13:42:06 +0000

    4

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
+    new_matrix = [
+        [],[],[]
+    ]
     for i in range(0,len(matrix)):
         for k in range(0,len(matrix[i])):
-            matrix[i][k] = matrix[i][k] ** 2
-    return matrix
+            new_matrix[i][k] = matrix[i][k] ** 2
+    return new_matrix
