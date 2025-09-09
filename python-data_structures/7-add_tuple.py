230b8e51b7aa8052aab36ee1d6e8548efad6b604
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-09 12:50:36 +0000

    4

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,12 +2,10 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) > len(tuple_b):
-        for k in range(0, len(tuple_a)):
-            if tuple_b[k] == None:
+        for k in range(len(tuple_b) - 1, len(tuple_a)):
                 tuple_b[k] = 0
     else:
-            for i in range(0, len(tuple_b)):
-                if tuple_a[i] == None:
+            for i in range(len(tuple_a) - 1, len(tuple_b)):
                     tuple_a[i] = 0
     tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return tuple_c
