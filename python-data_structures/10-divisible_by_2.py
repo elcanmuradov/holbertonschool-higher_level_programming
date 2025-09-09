194ef213675d044099bfd201ebf3312f18d90e20
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-09 13:20:55 +0000

    4

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
+    list = [True] * len(my_list)
     for i in range(0,len(my_list)):
         if my_list[i] % 2 == 0:
             list[i] = True
