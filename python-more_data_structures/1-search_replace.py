b6a6c4300fe210de703e0342875c1df2c2dba854
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-09 13:58:36 +0000

    4

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = [0] * len(my_list)
+    new_list = [] * len(my_list)
     for i in range(0, len(my_list)):
         if my_list[i] == search:
             new_list[i] = replace
