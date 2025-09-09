b3f7ec6c4c7ca3d08ca1fc81e636a83e6a9dcccf
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-09 13:12:28 +0000

    4

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,4 +3,8 @@
 def max_integer(my_list=[]):
     if my_list == []:
         return None
-    return my_list.sort().reverse()[0]
+    temp = my_list[0]
+    for i in range(1, len(my_list)):
+        if temp < my_list[i]:
+            temp = my_list
+    return temp
