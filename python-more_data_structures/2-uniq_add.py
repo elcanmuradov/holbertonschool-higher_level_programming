ff34d449bdc29ec305f959741920a5f2b2f25e43
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-09 14:08:56 +0000

    4

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -4,7 +4,7 @@ def uniq_add(my_list=[]):
     sum = 0
     uniq = []
     for i in my_list:
-        if not(i in uniq):
+        if not (i in uniq):
             sum += i
             uniq.append(i)
     return sum
