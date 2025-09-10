655449a829367b2fdaa34b516749014592cfdbf3
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-10 12:02:27 +0000

    4

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,9 +3,7 @@
 def multiply_by_2(a_dictionary):
     if a_dictionary is None:
         return None
-    
     new_dict = {}
     for key in a_dictionary:
         new_dict[key] = a_dictionary[key] * 2
-    
     return new_dict
