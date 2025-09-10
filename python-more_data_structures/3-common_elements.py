718228dbfcb7a625b956f51cfaf0acbf3cdad756
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-10 11:14:14 +0000

    4

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    new_list = []
+    for i in set_1:
+        for k in set_2:
+            if i == k:
+                new_list.append(i)
+    return new_list
+    
