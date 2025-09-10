968f0a638b858c2161547d9394f177637a30fe8c
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-10 12:20:06 +0000

    4

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -4,6 +4,4 @@ def multiply_list_map(my_list=[], number=0):
     if a_dictionary is None:
         return None
     new_dict = {}
-    for key in a_dictionary:
-        new_dict[key] = a_dictionary[key] * number
-    return new_dict
+    new_dict = map(pow, my_set, number)
