828a51ab0039ca1370fee3c4b61f49a2158dc1ea
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-10 12:08:40 +0000

    4

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return None
+        return "None"
     temp = 0
     for i in a_dictionary.keys():
         if temp < a_dictionary[i]:
