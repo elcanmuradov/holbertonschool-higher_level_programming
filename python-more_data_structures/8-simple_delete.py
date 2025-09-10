1c5332a93f3e35d30151df2c97b30fa72992899b
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-10 11:55:51 +0000

    4

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary is None or key is None:
-        return
-    return a_dictionary.pop(key)
+    if a_dictionary is None:
+        return a_dictionary
+    
+    if key in a_dictionary:
+        del a_dictionary[key]
+    
+    return a_dictionary
