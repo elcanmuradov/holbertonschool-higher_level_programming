29cdcc78996180910248a2b39e08d4785531eae1
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-10 11:43:10 +0000

    4

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,6 @@
 
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary is None:
-       return
+        return
     for key in sorted(a_dictionary.keys()):
         print("{}: {}".format(key, a_dictionary[key]))
