487c239ce9deae680ed1ccb96a716a9d56f91130
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-10 12:22:53 +0000

    4

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    if not my_list:
-        return []
-    return list(map(lambda x: x * number, my_list))
+    return None if not my_list else list(map(lambda x: x * number, my_list))
