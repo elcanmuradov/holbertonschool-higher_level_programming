53605c7b333a5d270790ad42d453dd4c09578597
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-10 12:17:47 +0000

    4

---
+++ python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiply_list_map(my_list=[], number=0):
+    if a_dictionary is None:
+        return None
+    new_dict = {}
+    for key in a_dictionary:
+        new_dict[key] = a_dictionary[key] * number
+    return new_dict
