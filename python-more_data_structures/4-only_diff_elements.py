cbd983865c13f07f11d5faff2baf16e8a587d5c7
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-10 11:24:10 +0000

    4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -8,3 +8,8 @@ def only_diff_elements(set_1, set_2):
         for k in set_2:
             if i == k:
                 common_list.append(i)
+    sum_list = set_1 + set_2
+    for a in sum_list:
+        for b in common_list:
+            if a != b:
+                new_list.append(a)
