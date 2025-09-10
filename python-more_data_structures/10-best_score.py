fe8b7fab04c47893e84e8b279b99c311c8576492
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-10 12:06:02 +0000

    4

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return
+    temp = a_dictionary[0]
+    for i in a_dictionary.keys:
+        if temp < a_dictionary[i]:
+            temp = a_dictionary[i]
+    return temp
--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -7,4 +7,3 @@ def multiply_by_2(a_dictionary):
     for key in a_dictionary:
         new_dict[key] = a_dictionary[key] * 2
     return new_dict
-    
