ecdef23f20167a8eca5d90797b81edf276424681
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-10 12:21:02 +0000

    4

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
+def mul4(number):
+    return number * 4
 def multiply_list_map(my_list=[], number=0):
     if a_dictionary is None:
         return None
     new_dict = {}
-    new_dict = map(pow, my_set, number)
+    new_dict = map(mul4, my_set)
