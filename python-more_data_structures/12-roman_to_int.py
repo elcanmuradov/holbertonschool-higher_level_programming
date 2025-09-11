f7ef3cdb0bd843456fb8a24345f52289e08db62a
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-11 12:05:02 +0000

    4

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 
 def change_number(num):
-    match num.upper():
-        case 'I':
-            return 1
-        case 'V':
-            return 5
-        case 'X':
-            return 10
-        case 'L':
-            return 50
-        case 'C':
-            return 100
-        case 'D':
-            return 500
-        case 'M':
-            return 1000
-        case _:
-            return -1
+    if num == 'I':
+        return 1
+    elif num == 'V':
+        return 5
+    elif num == 'X':
+        return 10
+    elif num == 'L':
+        return 50
+    elif num == 'C':
+        return 100
+    elif num == 'D':
+        return 500
+    elif num == 'M':
+        return 1000
+    else:
+        return -1
     
 # LXXXVII
 def roman_to_int(roman_string):
+    if roman_string is None or roman_string is int:
+        return None
     number = 0
     if len(roman_string) == 1:
         return change_number(roman_string)
