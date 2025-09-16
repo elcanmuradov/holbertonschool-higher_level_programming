41f266cad74c1968f70324c9db0788142b819551
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-16 13:40:17 +0000

    4

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Module that defines a Square class with a private size attribute."""
+"""Module that defines a Square class with validated size attribute."""
 
 
 class Square:
     """Class that defines a square."""
 
     def __init__(self, size):
-        """Initialize a new Square with a given size (no validation)."""
+        """Initialize a new Square with a given size, after validation."""
+
         if size is not int:
             raise TypeError("size must be an integer")
         if (size is int and size < 0){
