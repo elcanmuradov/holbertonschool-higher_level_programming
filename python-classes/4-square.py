aa462efde2a8463fd248b1418431ad543bab664d
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-16 13:59:47 +0000

    4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that defines a Square class with size validation and area calculation."""
+"""Module that defines a Square class with size validation and area."""
 
 
 class Square:
