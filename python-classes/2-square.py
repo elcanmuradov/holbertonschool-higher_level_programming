96ff0e7dd37996ea458b092a5bb35352a39312ce
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-16 13:43:31 +0000

    4

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Class that defines a square."""
     size = 0
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize a new Square with a given size, after validation."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
