9b3ed058bef40562e3bb53a1bab553e696acaec6
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-16 13:17:08 +0000

    4

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Module that defines an empty Square class with size."""
+
+
+class Square:
+        """The size of square"""
+    def __init__(self, size):
+        _size = size
