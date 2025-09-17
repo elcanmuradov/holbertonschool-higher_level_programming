f1f9147c3f228d9adf317d2f3a90250810fe1ee6
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-17 13:44:03 +0000

    5

---
+++ python-classes/6-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Module that defines a Square class with size validation and area."""
+
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size=0), position=(0, 0):
+        """Initialize a new Square with an optional size."""
+        self.size = size  # Use setter for validation
+
+    @property
+    def size(self):
+        """Get the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square, with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Get the current position of the square."""
+        return self.__position
+
+    @size.setter
+    def position(self, value):
+        """Set the position of the square, with validation."""
+        if not isinstance(value, int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__position = value
+
+    def area(self):
+        """Calculate and return the current area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print console the square"""
+        if self.__size == 0:
+            print()
+        for i in range(0, self.__size):
+            for k in range(0, self.__size):
+                print("#", end="")
+            print()
