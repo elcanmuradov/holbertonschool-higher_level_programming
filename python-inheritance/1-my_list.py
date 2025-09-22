baaa0282bbf10dff617bfbca4d73e25a2859f9c1
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-22 11:17:32 +0000

    3

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -10,5 +10,5 @@ class MyList(list):
 
     """Print the sorted list """
     def print_sorted(self):
-        self.newList = self.newList.sort()
-        print(self.newList)
+        newList = self.newList.sort()
+        print(newList)
