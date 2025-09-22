45f9f2212f65e56c54e18b6e05eb61bdf37c62a7
elcanmuradov <elcanmuradov25@gmail.com>
2025-09-22 11:12:50 +0000

    3

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,12 +3,12 @@
 
 
 class MyList(list):
-"""Class the defines new list"""
+    """Class the defines new list"""
     def __init__(self,newList=[]):
         """Initialize a new list"""
         newList = self.newList
 
-"""Print the sorted list """
+    """Print the sorted list """
     def print_sorted(self):
         newList = newList.sort()
         print(newList)
